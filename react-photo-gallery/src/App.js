2ae3063aff71a4fef7cd13c3b172e85d8ed8e3c2
Jason Ziegler <jason@insightfulautomation.com>
2021-06-06 14:48:23 -0500

    Removed Starter files and created SearchForm Component

--- react-photo-gallery/src/App.js
+++ react-photo-gallery/src/App.js
@@ -1,21 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
 
+
+// Components
+import SearchForm from './Components/SearchForm';
+
 function App() {
   return (
     <div className="container">
-      
-      <form className="search-form">
-        <input type="search" name="search" placeholder="Search" required/>
-        <button type="submit" className="search-button">
-          <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-            <path d="M0 0h24v24H0z" fill="none"/>
-          </svg>
-        </button>
-      </form>
+      {/*// Turn Search form into a Componenent*/}
+      {/* Check Data Fetching in React Search Form Component */}
+      <SearchForm />
 
       <nav className="main-nav">
+
+        {/* Use React Router for these?*/}
         <ul>
           <li><a href='#'>Cats</a></li>
           <li><a href='#'>Dogs</a></li>
@@ -23,6 +22,9 @@ function App() {
         </ul>
       </nav>
 
+
+      {/* Use Fetch to connect to Flickr API, and .then parse with .json(), .then this.setState,  .catch errors */}
+      {/*Turn photoContainer into a component, and create a photo component, pass data prop to PhotoContainer*/}
       <div className="photo-container">
         <h2>Results</h2>
         <ul>
@@ -39,6 +41,8 @@ function App() {
             <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
           </li>
           {/*<!-- Not Found -->*/}
+
+          {/* Great NotFound Component and display if empty results */}
           <li className="not-found">
             <h3>No Results Found</h3>
             <p>You search did not return any results. Please try again.</p>
